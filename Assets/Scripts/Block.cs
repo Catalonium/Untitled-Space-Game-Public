f5d696f216e2e675afb9228f34fbb23d5ad1115a
Catalonium <bulutcatikoglu@gmail.com>
2015-11-15 04:38:35 +0200

    Dev 2
    * Holo cursor added
    * Movement mode added
    * Collisions with other blocks are now detected while placing blocks (no
    block-inside-block)
    * Custom toolbar script updated

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class Block : BlockStruct {
+
+	public blockType blockType;
+	public float hull;
+	public float mass;
+	public float energyCon;
+	public float thrust;
+	public float maneuver;
+
+}
