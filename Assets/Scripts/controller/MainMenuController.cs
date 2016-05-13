f3737e89ccfd6ddd22c1d139fe8bb6ec1e0e98de
Catalonium <bulutcatikoglu@gmail.com>
2016-05-13 19:07:13 +0300

    Versioning update
    * Versioning update

--- Assets/Scripts/controller/MainMenuController.cs
+++ Assets/Scripts/controller/MainMenuController.cs
@@ -4,8 +4,7 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour {
 
 	void Start() {
-		GameObject.Find("GameRevision_Text").GetComponent<Text>().text = "Alpha Stage - revision 26";
-		GameObject.Find("GameVersion_Text").GetComponent<Text>().text = "Prototype Tech Demo v0.01";
+		GameObject.Find("GameVersion_Text").GetComponent<Text>().text = "Alpha Stage - Prototype Tech Demo v0.011";
 		GameObject.Find("GameInfo_Text").GetComponent<Text>().text = "2016 - Adnan Bulut Çatıkoğlu";
 	}
 
